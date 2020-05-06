ebee2970ba6d0e509f6885b0a21893f1cdc86692
gumobu <62066469+gumobu@users.noreply.github.com>
2020-05-06 12:55:48 +0300

    Fixed minor issues.
    Fixed weekday processing and punctuation marks usage. Added "Algorhitms" module import. The module is dedicated to uncertain response detection.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """"The main file of a project. Consists of main methods, including logging in as vk.com group"""
 
+import algorithms
 import data
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
@@ -16,6 +17,7 @@ class Keyboard:
     """
     Dedicated to creating keyboard
     """
+
     def __init__(self, response):
         self.response = response
         self.keyboard = VkKeyboard(one_time=False)
@@ -25,7 +27,7 @@ class Keyboard:
         Creates a keyboard for resending messages to chats
         """
         if self.response == '.логин 12681268':
-            self.keyboard.add_button('Отправить сообщение в <Chat1>',
+            self.keyboard.add_button('Отправить сообщение в <Chat1>')
                                      color=VkKeyboardColor.PRIMARY, payload='2')
             self.keyboard.add_line()
             self.keyboard.add_button('Отправить сообщение в <Chat2>',
@@ -42,14 +44,14 @@ class Messages:
     Processes messages. Re- and just sends them. Gets picture by using logging in as user
     """
     def __init__(self, peer_id=None, message=None, attachment=None, keyboard=None,
-                 vk_session=None, text=None, num=None,
+                 session=None, text=None, num=None,
                  event=None,
                  response=None, admin=False):
         self.peer_id = peer_id
         self.message = message
         self.attachment = attachment
         self.keyboard = keyboard
-        self.vk_session = vk_session
+        self.vk_session = session
         self.text = text
         self.num = num
         self.event = event
@@ -66,13 +68,13 @@ class Messages:
 
     def resend(self):
         """
-        Resends messages
+        Reends messages
         """
         if self.num == 1:  # Info chat
-            return vk_session.method('messages.send', {'peer_id': 2000000003, 'message': self.text,
+            return vk_session.method('messages.send', {'peer_id': <Chat1Id>, 'message': self.text,
                                                        'random_id': random.randint(-2147483648, +2147483648)})
         elif self.num == 2:  # Main chat
-            return vk_session.method('messages.send', {'peer_id': 2000000002, 'message': self.text,
+            return vk_session.method('messages.send', {'peer_id': <Chat2Id>, 'message': self.text,
                                                        'random_id': random.randint(-2147483648, +2147483648)})
 
     def name_determination(self):
@@ -96,13 +98,19 @@ class Messages:
             text = session_api.messages.getHistory(peer_id=<AdminId>)['items'][1]['text']
             Messages(peer_id=<AdminId>, message=f'Сообщение с текстом "{text}"'
                                                 f' успешно отправлено в беседу СПИДИНФО', keyboard=keyboard).send()
-            Messages(vk_session=vk_session, text=text, num=1).resend()
+            Messages(session=vk_session, text=text, num=1).resend()
         if payload == 3:
             keyboard = VkKeyboard.get_empty_keyboard()
             text = session_api.messages.getHistory(peer_id=<AdminId>)['items'][1]['text']
             Messages(peer_id=<AdminId>, message=f'Сообщение с текстом "{text}"'
                                                 f' успешно отправлено в беседу БАП1901', keyboard=keyboard).send()
-            Messages(vk_session=vk_session, text=text, num=2).resend()
+            Messages(session=vk_session, text=text, num=2).resend()
+        if payload == 4:
+            keyboard = VkKeyboard.get_empty_keyboard()
+            text = session_api.messages.getHistory(peer_id=<AdminId>)['items'][1]['text']
+            Messages(peer_id=<AdminId>, message=f'Сообщение с текстом "{text}"'
+                                                f' успешно отправлено в тестовую беседу', keyboard=keyboard).send()
+            Messages(session=vk_session, text=text, num=0).resend()
 
     @staticmethod
     def response_determination(admin, response, event, keyboard):
@@ -110,8 +118,8 @@ class Messages:
         Determines responses and processes incoming messages
         """
         if admin is False:  # If not a private message from bot owner
-            if response == '.команды':  # If not a private message from bot owner
-                name = Messages(event=event).name_determination()
+            name = Messages(event=event).name_determination()
+            if response == '.команды':  # Sends a list of comands
                 Messages(peer_id=event.message.peer_id,
                          message=f'Привет, {name}! \nВот команды для бота, '
                                  f'их регистр не важен:'
@@ -127,24 +135,25 @@ class Messages:
 
             elif response == '.расп':  # Send a picture of timetable
                 Messages(peer_id=event.message.peer_id,
-                         message=f'Актуальное на '
+                         message=f'Привет, {name}! Актуальное на '
                                  f'{DateAndTime().now_cls.strftime("%d.%m.%Y")} расписание ',
                          attachment=user_actions.Pictures.get_single('-' + data.home_id, user_actions.session_api,
                                                                      0)).send()
 
             elif response == '.список':  # Sends a picture of a group members list
                 Messages(peer_id=event.message.peer_id,
-                         message=f'Актуальный на {DateAndTime().now_cls.strftime("%d.%m.%Y")} список группы',
+                         message=f'Привет, {name}! Актуальный на {DateAndTime().now_cls.strftime("%d.%m.%Y")}'
+                                 f' список группы',
                          attachment=user_actions.Pictures.get_single('-' + data.home_id,
                                                                      user_actions.session_api, 1)).send()
 
             elif response == '.мем':  # Sends funny picture(s)
-                Messages(peer_id=event.message.peer_id, message='Ваши мемы прибыли, капитан!',
+                Messages(peer_id=event.message.peer_id, message=f'Ваши мемы прибыли, капитан {name}!',
                          attachment=user_actions.Pictures.get_multiple(user_actions.session_api)).send()
 
             elif response == '.дист':  # Temporary function. Sends a distance learning timetable
                 Messages(peer_id=event.message.peer_id,
-                         message=f'Расписание дистанционных занятий,'
+                         message=f'Привет, {name}! Расписание дистанционных занятий,'
                                  f' актуальное на {DateAndTime().now_cls.strftime("%d.%m.%Y")}',
                          attachment=user_actions.Pictures.get_single('-' + data.home_id,
                                                                      user_actions.session_api, 7)).send()
@@ -173,6 +182,11 @@ class DateAndTime:
         self.current_date = date.today()
         self.time_param = time_param
         self.start_study = datetime.date(datetime.strptime(data.start_study, '%d.%m.%Y'))
+        self.get_hour_str = None
+        self.add_minute_str = None
+        self.add_hour_str = None
+        self.delta_minute = None
+        self.delta_hour = None
 
     def week_counter(self):
         """
@@ -192,19 +206,21 @@ class DateAndTime:
         if self.start is False:
             if datetime.strptime(data.end_time[int(self.id)], '%H:%M').replace(tzinfo=None).minute \
                     - self.time_param.replace(tzinfo=None).minute < 0:
-                return datetime.strptime(data.end_time[int(self.id)], '%H:%M').replace(tzinfo=None).hour \
-                       - self.time_param.replace(tzinfo=None).hour - 1
+                self.delta_hour = datetime.strptime(data.end_time[int(self.id)], '%H:%M').replace(tzinfo=None).hour \
+                                - self.time_param.replace(tzinfo=None).hour - 1
             else:
-                return datetime.strptime(data.end_time[int(self.id)], '%H:%M').replace(tzinfo=None).hour \
-                       - self.time_param.replace(tzinfo=None).hour
+                self.delta_hour = datetime.strptime(data.end_time[int(self.id)], '%H:%M').replace(tzinfo=None).hour \
+                                - self.time_param.replace(tzinfo=None).hour
         else:
             if datetime.strptime(data.start_time[int(self.id)], '%H:%M').replace(tzinfo=None).minute \
                     - self.time_param.replace(tzinfo=None).minute < 0:
-                return int(datetime.strptime(data.start_time[int(self.id)], '%H:%M').replace(tzinfo=None).hour) \
-                       - self.time_param.replace(tzinfo=None).hour - 1
+                self.delta_hour = int(datetime.strptime(data.start_time[int(self.id)],
+                                                        '%H:%M').replace(tzinfo=None).hour) \
+                                - self.time_param.replace(tzinfo=None).hour - 1
             else:
-                return datetime.strptime(data.start_time[int(self.id)], '%H:%M').replace(tzinfo=None).hour \
-                       - self.time_param.replace(tzinfo=None).hour
+                self.delta_hour = datetime.strptime(data.start_time[int(self.id)], '%H:%M').replace(tzinfo=None).hour \
+                                - self.time_param.replace(tzinfo=None).hour
+        return int(self.delta_hour)
 
     def get_minute(self):
         """
@@ -224,7 +240,7 @@ class DateAndTime:
                 self.delta_minute = 60 - self.time_param.replace(tzinfo=None).minute \
                                     + datetime.strptime(data.start_time[int(self.id)],
                                                         '%H:%M').replace(tzinfo=None).minute
-        return self.delta_minute
+        return int(self.delta_minute)
 
     def time_to(self):
         to_start, to_end = None, None
@@ -267,7 +283,6 @@ class Classes(DateAndTime):
         Sets a correct case for minutes count
         """
         self.minute = Classes(time_param=self.time_param, element_id=self.id, start=self.start).get_minute()
-        print(str(self.minute)[-1])
         if str(self.minute).endswith('1') and self.minute != 11:
             self.add_minute_str = 'минута'
         elif str(self.minute).endswith('2' or '3' or '4') and self.minute not in (12, 13, 14):
@@ -278,28 +293,28 @@ class Classes(DateAndTime):
 
     def hurry_up(self):
         """"
-        Adds some additional words, depending on the time left to class' start or end
+        Adds some additional words, depending on the time left to class' start
         """
         self.minute = Classes(time_param=self.time_param, element_id=self.id, start=self.start).get_minute()
         self.hour = Classes(time_param=self.time_param, element_id=self.id, start=self.start).get_hour()
         if self.start:
             if self.hour >= 1:
-                return ' Врeмени хватает'
+                return ' Врeмени хватает.'
             elif 60 > self.minute >= 30:
-                return ' Времени хватает. '
+                return ' Времени хватает.'
             elif 30 > self.minute >= 15:
-                return ' Времени на раскачку нет. '
+                return ' Времени на раскачку нет.'
             elif 15 > self.minute >= 5:
-                return ' Не прозевай начало. '
+                return ' Не прозевай начало.'
             elif 5 > self.minute >= 1:
-                return ' Совсем скоро начнется'
+                return ' Совсем скоро начнется!'
             else:
                 print(self.minute)
-                return ' Осталось меньше минуты. Поторопись!'
+                return ' Осталось меньше минуты. Поторопись.'
 
         else:
             if self.hour >= 1:
-                return 'Пара только началась.'
+                return 'Пара только началась...'
             elif 60 > self.minute >= 30:
                 return 'Осталось совсем недолго!'
             elif 30 > self.minute >= 15:
@@ -307,17 +322,18 @@ class Classes(DateAndTime):
             elif 15 > self.minute >= 5:
                 return 'Идут последние минуты.'
             elif 5 > self.minute >= 1:
-                return 'Остались считанные секунды'
+                return 'Остались считанные секунды.'
             else:
                 return 'Почему ты об этом спрашиваешь?'
 
     def word_total(self):
         """
         Returns time left till start or end and hurries up
-        """
-        return Classes(time_param=self.time_param, element_id=self.id, start=self.start).word_hour() +\
-               Classes(time_param=self.time_param, element_id=self.id, start=self.start).word_minute() +\
-               '. ' + Classes(time_param=self.time_param, element_id=self.id, start=self.start).hurry_up() + '.\n'
+         """
+        return Classes(time_param=self.time_param, element_id=self.id, start=self.start).word_hour() \
+               + Classes(time_param=self.time_param, element_id=self.id, start=self.start).word_minute() \
+               + '. ' \
+               + Classes(time_param=self.time_param, element_id=self.id, start=self.start).hurry_up() + '\n'
 
     def processing(self):
         """
@@ -330,7 +346,7 @@ class Classes(DateAndTime):
             odd = is_odd(self.week_counter())
 
             if day_id == (6 or 7):  # If it is saturday or sunday
-                if is_odd(DateAndTime().week_counter()) is True:
+                if odd is True:
                     return (f'Сегодня {data.day_names[day_id]}, '
                             f'следующая пара в {data.day_names[1]}. \n'
                             f'Это {data.classes[0]["list_of_classes"][0]}.\n'
@@ -340,7 +356,7 @@ class Classes(DateAndTime):
                     if self.week_counter() < 10:
                         return f'Сегодня {data.day_names[day_id]}, ' \
                                f'следующая пара в {data.day_names[1]}. \n' \
-                               f'Это {data.classes[1]["list_of_classes"][1]}.\n' \
+                               f'Это {data.classes[1]["list_of_classes"][0]}.\n' \
                                f'Начнется в {data.start_time[data.classes[1]["num_of_classes"][0]]} \n' \
                                f'Можно отдыхать.'
                     else:
@@ -482,9 +498,8 @@ class Classes(DateAndTime):
                                f'Завтра в {data.start_time[data.classes[6]["num_of_classes"][0]]}'
 
             if day_id == 4:  # If it is thursday
-                start = 1
-                for _ in range(start, len(data.classes[5]["num_of_classes"]), 1):
-                    start_true, end_true = DateAndTime(time_param=today, element_id=_).time_to()
+                for _ in range(0, len(data.classes[5]["num_of_classes"])):
+                    start_true, end_true = DateAndTime(time_param=today, element_id=_ + 1).time_to()
                     if start_true or end_true is True:
                         break
 
@@ -492,27 +507,27 @@ class Classes(DateAndTime):
                     if self.week_counter() < 6:
                         return f'Следующая пара: {data.classes[5]["list_of_classes"][_]}.\n' \
                                f'До ее начала осталось' \
-                               f' {Classes(time_param=today, element_id=_, start=True).word_total()}'
+                               f' {Classes(time_param=today, element_id=_ + 1, start=True).word_total()}'
                     else:
                         return f'Следующая пара: {data.classes[6]["list_of_classes"][_]}.\n' \
                                f'До ее начала осталось ' \
-                               f'{Classes(time_param=today, element_id=_, start=True).word_total()}'
+                               f'{Classes(time_param=today, element_id=_ + 1, start=True).word_total()}'
                 if end_true is True:
                     if _ < len(data.classes[5]["num_of_classes"]) - 1:
                         if self.week_counter() < 6:
                             return f'Текущая пара: {data.classes[5]["list_of_classes"][_]}.\n' \
-                                   f'До ее конца осталось {Classes(time_param=today, element_id=_).word_total()}\n' \
+                                   f'До ее конца осталось {Classes(time_param=today, element_id=_ + 1).word_total()}\n'\
                                    f'Следующая пара: {data.classes[5]["list_of_classes"][_ + 1]}.\n' \
                                    f'Она начнется в {data.start_time[data.classes[5]["num_of_classes"][_ + 1]]}'
                         else:
                             return f'Текущая пара: {data.classes[6]["list_of_classes"][_]}.\n' \
-                                   f'До ее конца осталось {Classes(time_param=today, element_id=_).word_total()}\n' \
+                                   f'До ее конца осталось {Classes(time_param=today, element_id=_ + 1).word_total()}\n'\
                                    f'Следующая пара: {data.classes[6]["list_of_classes"][_ + 1]}.\n' \
                                    f'Она начнется в {data.start_time[data.classes[6]["num_of_classes"][_ + 1]]}'
 
                     elif _ == len(data.classes[5]["num_of_classes"]) - 1:
                         return f'Текущая пара: {data.classes[5]["list_of_classes"][_]}.\n' \
-                               f'До ее конца осталось {Classes(time_param=today).word_total()}\n' \
+                               f'До ее конца осталось {Classes(time_param=today, element_id=_ + 1).word_total()}\n' \
                                f'Следующая пара: {data.classes[7]["list_of_classes"][0]}.\n' \
                                f'Завтра в {data.start_time[data.classes[7]["num_of_classes"][0]]}.'
                 else:
@@ -609,9 +624,13 @@ class Classes(DateAndTime):
                                f'В {data.day_names[1]} ' \
                                f'в {data.start_time[data.classes[0]["num_of_classes"][0]]}\n' \
                                f'Можно отдыхать!'
+        else:
+            return 'Сегодня каникулы, ни о какой учебе не может быть и речи!'
 
-
-def is_odd(param):  # Finds out if the parameter odd or not
+def is_odd(param):
+    """
+    Finds out if the parameter odd or not
+    """
     if param % 2 == 0:
         return True
     else:
@@ -626,6 +645,7 @@ def main(events):  # Main function
                 response = event.message.text.lower()
                 keyboard = Keyboard(response).create_main()
                 if int(event.message.peer_id) != 233071173:
+                    response = algorithms.main(response, data.commands)
                     Messages().response_determination(admin=False, response=response, event=event, keyboard=keyboard)
                 else:
                     Messages().response_determination(admin=True, response=response, event=event, keyboard=keyboard)
